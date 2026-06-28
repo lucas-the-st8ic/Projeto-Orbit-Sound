b192a4a4a2b624e63ef83e54c163fd0fd720c5bd
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-06-28 14:46:09 -0300

    Initial commit

---
+++ orbitsound/src/main/java/com/orbitsound/OrbitsoundApplication.java
@@ -0,0 +1,13 @@
+package com.orbitsound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitsoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitsoundApplication.class, args);
+	}
+
+}
---
+++ orbitsound/src/test/java/com/orbitsound/OrbitsoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbitsound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitsoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
